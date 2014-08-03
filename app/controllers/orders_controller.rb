4fe76616ae48c56553503bc6c350b0c41505771c
kiwi <jcwen@thoughtworks.com>
2014-08-03 20:03:16 +0800

    setup get single order

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -5,6 +5,11 @@ class OrdersController < ApplicationController
 		@orders = @user.orders
 	end
 
+	def show
+		@order = @user.orders.select { |order| order._id == BSON::ObjectId.from_string(params[:id]) }.first
+		render nothing: true
+	end
+
 	private
 		def get_user
 			@user = User.find(params[:user_id])
--- spec/orders/orders_controller_spec.rb
+++ spec/orders/orders_controller_spec.rb
@@ -34,4 +34,25 @@ describe OrdersController, :type => :controller do
 		end
 	end
 
+	describe 'GET order' do
+		context 'when order exist' do
+			before(:each) do
+				@orders = [
+					{_id: BSON::ObjectId.from_string('53c28cfa39fb280037000001'), shipping_address: 'tianfu1', phone: '13880660444'},
+					{_id: BSON::ObjectId.from_string('53c28cfa39fb280037000002'), shipping_address: 'tianfu2', phone: '13880660555'}
+				]
+
+				@user = User.new({_id: "1", name: "uesr1", contact_address: "chengdu", orders: @orders})
+
+				expect(User).to receive(:find).with("1").and_return(@user).once
+
+				get :show, format: :json, user_id: 1, id: '53c28cfa39fb280037000001'
+			end
+
+			it 'have http status 200' do
+				expect(response).to have_http_status(200)
+			end
+		end
+	end
+
 end
